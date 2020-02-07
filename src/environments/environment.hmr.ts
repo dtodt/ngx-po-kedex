45f681d9186dc74102f09f1d39e523d3c82dd2ef
dtodt <danieltodt@gmail.com>
2020-02-07 17:43:02 -0300

    chore: enviroment updated

--- src/environments/environment.hmr.ts
+++ src/environments/environment.hmr.ts
@@ -1,4 +1,6 @@
 export const environment = {
+  pokeApiBaseUrl: 'https://pokeapi.co/api/v2/',
+  pokeApiSpriteUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
   production: false,
   hmr: true
 };
