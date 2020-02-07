1cc03fd35473bf979cfa2e5cafe591965612e62f
dtodt <danieltodt@gmail.com>
2020-02-07 17:52:57 -0300

    feat: pokeapi simple get

---
+++ src/app/poke-api.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PokeApiService } from './poke-api.service';
+
+describe('PokeApiService', () => {
+  let service: PokeApiService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ]
+    });
+    service = TestBed.inject(PokeApiService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/poke-api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { PokemonListResult } from '@app/models';
+import { environment } from '@environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeApiService {
+
+  constructor(private http: HttpClient) {}
+
+  getPokemonList(offset: number = 0, limit: number = 20): Observable<PokemonListResult> {
+    return this.http.get<any>(`${environment.pokeApiBaseUrl}pokemon?offset=${offset}&limit=${limit}`);
+  }
+
+}
