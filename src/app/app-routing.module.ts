6391c7a003add8ca38987c36cf2889278c2e61ad
dtodt <danieltodt@gmail.com>
2020-02-08 12:00:12 -0300

    fix: routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/national', pathMatch: 'full' },
   {
-    path: '',
+    path: 'national',
     loadChildren: () =>
       import('./national/national.module').then(m => m.NationalModule)
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./not-found/not-found.module').then(m => m.NotFoundModule)
   }
 ];
 
