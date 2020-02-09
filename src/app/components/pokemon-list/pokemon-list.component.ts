d69f06dd9afbbf0f53cdfa0a06f9305b834c2b16
dtodt <danieltodt@gmail.com>
2020-02-09 00:06:09 -0300

    feat: pokemon list tile propagation

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { PokemonListModel } from '@app/models';
+import { FavoritePokemonMap } from '@app/state/app.state';
 
+/**
+ * Wraps all cards together.
+ */
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.sass']
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent {
+  @Input() favorites: FavoritePokemonMap;
   @Input() pokemons: PokemonListModel[];
+  /**
+   * Emit events when the card is clicked.
+   */
+  @Output() displayDetails = new EventEmitter<number>();
+  /**
+   * Emit events when the star is clicked.
+   */
+  @Output() toggleFavorite = new EventEmitter<number>();
 
-  constructor() {}
+  /**
+   * Emitting events to keep the component as simple as possible.
+   */
+  cardClicked(id?: number): void {
+    this.displayDetails.emit(id);
+  }
 
-  ngOnInit(): void {}
+  starClicked(id?: number): void {
+    this.toggleFavorite.emit(id);
+  }
 }
