e2b5c3a0ae1045735b0d0bcfc87e880edf4dbbb7
dtodt <danieltodt@gmail.com>
2020-02-09 15:37:16 -0300

    chore: sprite api ext

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   pokeApiBaseUrl: 'https://pokeapi.co/api/v2/',
-  pokeApiSpriteUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
+  pokeApiSpriteUrl:
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
+  pokeApiSpriteExt: '.png',
   production: true,
   hmr: false
 };
