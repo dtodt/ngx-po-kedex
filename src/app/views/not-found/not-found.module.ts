39ca932a174462e93fd87adc1449868dae3b8943
dtodt <danieltodt@gmail.com>
2020-02-09 23:45:31 -0300

    chore: error page working

--- src/app/views/not-found/not-found.module.ts
+++ src/app/views/not-found/not-found.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { NotFoundRoutingModule } from './not-found-routing.module';
 import { NotFoundComponent } from './not-found.component';
 
 @NgModule({
   declarations: [NotFoundComponent],
-  imports: [CommonModule, NotFoundRoutingModule]
+  imports: [CommonModule, FlexLayoutModule, NotFoundRoutingModule]
 })
 export class NotFoundModule {}
